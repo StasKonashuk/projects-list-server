e09608e0686202604b9ebd1dcc1bf5c9054f9a96
Stanislav <skonashuk@list.ru>
2022-01-11 21:28:33 +0300

    Connect database to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const rootRouter = require('./routes');
+const db = require('./db/db');
 
 const PORT = process.env.PORT || 5000;
 
@@ -10,6 +12,12 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', rootRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+app.listen(PORT, async () => {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+    console.log(`Server started on port ${PORT}`);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 });

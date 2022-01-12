f6dbb6c253e3557774e0651182b62d2ec75308b9
Stanislav <skonashuk@list.ru>
2022-01-12 13:41:29 +0300

    Add create, update, get projects controllers

--- controller/projectsController.js
+++ controller/projectsController.js
@@ -3,8 +3,15 @@ const { Projects, Tasks } = require('../models');
 class ProjectsController {
   async getProjects(req, res) {
     try {
-      const projects = await Projects.findAll();
-      res.status(200).json(projects);
+      await Projects.findAll({
+        include: ['tasks'],
+      })
+        .then((projects) => {
+          return res.status(200).json(projects);
+        })
+        .catch((error) => {
+          return res.json({ error: error.message });
+        });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
@@ -12,6 +19,52 @@ class ProjectsController {
 
   async createProject(req, res) {
     const {
+      id,
+      companyName,
+      email,
+      projectAuthor,
+      projectDescripton,
+      projectLanguage: { language },
+      projectName,
+      projectTasks,
+      projectTitle,
+      projectVersionSystemControl,
+    } = req.body;
+
+    try {
+      await Projects.create({
+        project_id: id,
+        project_name: projectName,
+        project_title: projectTitle,
+        project_description: projectDescripton,
+        project_author: projectAuthor,
+        project_version_system_control: projectVersionSystemControl,
+        email,
+        company_name: companyName,
+        project_language: language,
+      })
+        .then(() => {
+          for (const task of projectTasks) {
+            Tasks.create({
+              task_id: task.id,
+              task_name: task.name,
+              task_status: task.status,
+              project_id: id,
+            });
+          }
+          res.status(200).json({ message: 'User created successfully' });
+        })
+        .catch((error) => {
+          return res.status(400).json({ error: error.message });
+        });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+
+  async updateProject(req, res) {
+    const {
+      id,
       companyName,
       email,
       projectAuthor,
@@ -23,31 +76,34 @@ class ProjectsController {
       projectVersionSystemControl,
     } = req.body;
     try {
-      await Projects.create({
-        project_name: projectName,
-        project_title: projectTitle,
-        project_description: projectDescripton,
-        project_author: projectAuthor,
-        project_version_system_control: projectVersionSystemControl,
-        email,
-        company_name: companyName,
-        project_language: language,
-      });
-      for (const task of projectTasks) {
-        Tasks.create({
-          task_name: task.name,
-          task_status: task.status,
+      await Projects.update(
+        {
+          project_name: projectName,
+          project_title: projectTitle,
+          project_description: projectDescripton,
+          project_author: projectAuthor,
+          project_version_system_control: projectVersionSystemControl,
+          email,
+          company_name: companyName,
+          project_language: language,
+        },
+        { returning: true, where: { project_id: id } }
+      )
+        .then(() => {
+          for (const task of projectTasks) {
+            Tasks.update(
+              {
+                task_name: task.name,
+                task_status: task.status,
+              },
+              { returning: true, where: { task_id: task.id } }
+            );
+          }
+          res.status(200).json({ message: 'Project updated successfully' });
+        })
+        .catch((error) => {
+          return res.status(400).json({ error: error.message });
         });
-      }
-      res.status(200).json({ message: 'Project created successfully' });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  }
-
-  updateProject(req, res) {
-    try {
-      console.log('Update');
     } catch (error) {
       res.status(500).json({ error: error.message });
     }

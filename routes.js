01ce63e045f9379010ff48e89a2e748f151fd37e
Stanislav <skonashuk@list.ru>
2022-01-12 16:53:18 +0300

    Add middleware to create and update routes

--- routes.js
+++ routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const projectsController = require('./controller/projectsController');
+const validator = require('./middleware/validation');
 
 const rootRouter = express.Router();
 
 rootRouter.get('/projects', projectsController.getProjects);
-rootRouter.post('/create-project', projectsController.createProject);
-rootRouter.put('/update-project', projectsController.updateProject);
+rootRouter.post('/create-project', validator, projectsController.createProject);
+rootRouter.put('/update-project', validator, projectsController.updateProject);
 
 module.exports = rootRouter;

57e5772039292d434679aebc64b64e8f2544daa8
Stanislav <skonashuk@list.ru>
2022-01-14 15:53:04 +0300

    Fix db model

--- models/index.js
+++ models/index.js
@@ -29,18 +29,9 @@ fs.readdirSync(__dirname)
     );
   })
   .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize);
     db[model.name] = model;
   });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
